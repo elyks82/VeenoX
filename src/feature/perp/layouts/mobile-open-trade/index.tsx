b9d72f17bb48a615bf9bb5938f89d57d2b0694f9
Veeno <sebastien.cloiseau@gmail.com>
2024-08-13 12:01:17 +0200

    fix: drawer position

--- src/feature/perp/layouts/mobile-open-trade/index.tsx
+++ src/feature/perp/layouts/mobile-open-trade/index.tsx
@@ -1,5 +1,6 @@
 import { useGeneralContext } from "@/context";
 import { FuturesAssetProps } from "@/models";
+import { useRef } from "react";
 import { OpenTrade } from "../open-trade";
 import { Orderbook } from "../orderbook";
 import { TriggerMobileTradeCreator } from "./trigger";
@@ -11,6 +12,11 @@ type MobileOpenTradeProps = {
 export const MobileOpenTrade = ({ asset }: MobileOpenTradeProps) => {
   const { showMobileTradeCreator, setShowMobileTradeCreator } =
     useGeneralContext();
+  const tradeCreatorRef = useRef<HTMLDivElement>(null);
+  const getPositionFromContainerRef = () => {
+    const clientHeight = tradeCreatorRef?.current?.clientHeight || 0;
+    return `calc(100% - ${clientHeight}px)`;
+  };
   return (
     <>
       <div
@@ -22,9 +28,11 @@ export const MobileOpenTrade = ({ asset }: MobileOpenTradeProps) => {
         } transition-all duration-200 ease-in-out bg-secondary z-30`}
       />
       <div
-        className={`fixed bottom-0 h-fit w-full md:w-[350px] z-[100] left-0 ${
-          showMobileTradeCreator ? "-translate-y-0" : "translate-y-full"
-        } transition-all duration-200 ease-in-out bg-secondary border-t border-borderColor shadow-2xl flex`}
+        ref={tradeCreatorRef}
+        style={{
+          top: showMobileTradeCreator ? getPositionFromContainerRef() : "100%",
+        }}
+        className={`fixed h-fit w-full md:w-[350px] z-[100] left-0  transition-all duration-200 ease-in-out bg-secondary border-t border-borderColor shadow-2xl flex`}
       >
         <OpenTrade isMobile />
         <Orderbook asset={asset} isMobileOpenTrade isMobile />
