bec2f185c83c7b6d8dce14253c65ab51698a0803
Veeno <sebastien.cloiseau@gmail.com>
2024-08-17 21:01:36 +0200

    fix: ts error

--- src/feature/perp/layouts/open-trade/hook.tsx
+++ src/feature/perp/layouts/open-trade/hook.tsx
@@ -1,7 +1,12 @@
+import { FuturesAssetProps } from "@/models";
 import { useMaxQty } from "@orderly.network/hooks";
 import { OrderSide } from "@orderly.network/types";
 
-export const CreateOrder = ({ asset }) => {
+type CreateOrder = {
+  asset: FuturesAssetProps;
+};
+
+export const CreateOrder = ({ asset }: CreateOrder) => {
   const maxQty: number = useMaxQty(asset?.symbol, OrderSide.BUY);
 
   console.log("maxQtu", maxQty);
