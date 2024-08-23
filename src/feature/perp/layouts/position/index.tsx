2bae6d4547d1a6a681b0cd159977725edbf888e7
Veeno <sebastien.cloiseau@gmail.com>
2024-08-23 19:21:25 +0200

    feature: handle position pending

--- src/feature/perp/layouts/position/index.tsx
+++ src/feature/perp/layouts/position/index.tsx
@@ -104,6 +104,12 @@ export const Position = ({ asset }: PositionProps) => {
   console.log("stream", data, orders);
   console.log("ActiveSection", activeSection);
 
+  const filterSide = (entry) => {
+    if (activeSection === 1)
+      return entry?.total_executed_quantity !== entry?.quantity;
+    return true;
+  };
+
   return (
     <div className="w-full">
       <div className="w-full flex justify-between items-center border-b border-borderColor ">
@@ -178,7 +184,7 @@ export const Position = ({ asset }: PositionProps) => {
               ? (data?.rows?.length as number) > 0
                 ? data.rows
                 : Array.from({ length: 1 })
-              : orders
+              : orders?.filter(filterSide)
             )?.map((order, i) => {
               if (
                 (activeSection === 0 && !data?.rows?.length) ||
@@ -199,6 +205,7 @@ export const Position = ({ asset }: PositionProps) => {
                   </div>
                 );
               }
+
               return (
                 <tr key={i}>
                   {renderCommonCells(order)}
@@ -221,13 +228,13 @@ const renderCommonCells = (trade: any) => (
           className="w-4 h-4 rounded-full mr-2"
           height={16}
           width={16}
-          alt={`${trade.symbol} logo`}
+          alt={`${trade?.symbol} logo`}
           src={`https://oss.orderly.network/static/symbol_logo/${formatSymbol(
-            trade.symbol,
+            trade?.symbol ? trade?.symbol : "PERP_TEST_TECJ",
             true
           )}.png`}
         />
-        {formatSymbol(trade.symbol)}
+        {trade?.symbol ? formatSymbol(trade.symbol) : "TEST"}
       </div>
     </td>
   </>
@@ -268,30 +275,50 @@ const renderAdditionalCells = (
       </>
     );
   } else if (section === Sections.PENDING) {
+    const toPercentage = (): number => {
+      if (trade?.quantity === 0) return 0;
+      return (trade?.total_executed_quantity / trade?.quantity) * 100;
+    };
+    const percentageFilled = toPercentage();
     return (
       <>
-        <td className={tdStyle}>{trade.type}</td>
+        <td className={tdStyle}>{trade?.type}</td>
         <td
           className={cn(
             tdStyle,
-            `${trade.side === "SELL" ? "text-red" : "text-green"}`
+            `${trade?.side === "SELL" ? "text-red" : "text-green"}`
           )}
         >
-          {trade.side}
+          {trade?.side}
         </td>
-        <td className={tdStyle}>--</td>
-        <td className={tdStyle}>{trade.total_executed_quantity}</td>
-        <td className={tdStyle}>{trade.average_executed_price}</td>
         <td className={tdStyle}>--</td>
         <td className={tdStyle}>
-          {getFormattedAmount(
-            trade.total_executed_quantity * trade.average_executed_price
-          )}
+          <div className="w-full h-full flex flex-col items-end">
+            <div className="h-[5px] w-[100px] rounded bg-terciary">
+              <div
+                className={`h-full bg-base_color rounded`}
+                style={{
+                  width: `${percentageFilled}%`,
+                }}
+              />
+            </div>
+            <div className="flex items-center justify-between w-[100px] mt-1.5">
+              <p className="text-white text-[11px]">
+                {trade?.total_executed_quantity || 0} / {trade?.quantity || 0}
+              </p>
+              <p className="text-white text-[11px]">{percentageFilled || 0}%</p>
+            </div>
+          </div>
+        </td>
+        <td className={tdStyle}>{trade?.price}</td>
+        <td className={tdStyle}>--</td>
+        <td className={tdStyle}>
+          {getFormattedAmount(trade?.quantity * trade?.price)}$
         </td>
         <td className={tdStyle}>No</td>
-        <td className={tdStyle}>{trade.reduce_only ? "Yes" : "No"}</td>
+        <td className={tdStyle}>{trade?.reduce_only ? "Yes" : "No"}</td>
         <td className={cn(tdStyle, "pr-5")}>
-          {getFormattedDate(trade.created_time)}
+          {getFormattedDate(trade?.created_time)}
         </td>
       </>
     );
