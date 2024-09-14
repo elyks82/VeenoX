bd87f9c8561bb42a835ce3b0b75d1afff6304bad
Veeno <sebastien.cloiseau@gmail.com>
2024-09-14 09:58:10 +0200

    feature: new total value section

--- src/modals/leverage/index.tsx
+++ src/modals/leverage/index.tsx
@@ -7,6 +7,7 @@ import {
 } from "@/lib/shadcn/dialog";
 import { useLeverage, useMarginRatio } from "@orderly.network/hooks";
 import { useRef } from "react";
+import { FaRegEdit } from "react-icons/fa";
 import { toast } from "react-toastify";
 import { LeverageEditor } from "./editor";
 
@@ -46,10 +47,16 @@ export const Leverage = () => {
   return (
     <Dialog>
       <DialogTrigger>
-        <button className="text-white">YO BROROROROGOOR</button>
+        <button className="text-white flex flex-col justify-center items-end">
+          <p className="text-font-60 text-xs mb-[3px]">Account Leverage</p>
+          <div className="flex items-center text-sm text-white hover:text-base_color transition-color duration-150 ease-in-out">
+            <p>{currentLeverage.toFixed(2)}x</p>/ <p>{maxLeverage}x</p>{" "}
+            <FaRegEdit className="ml-2" />
+          </div>
+        </button>
       </DialogTrigger>
       <DialogContent
-        className="max-w-[450px] w-[90%] h-auto max-h-[90vh] flex flex-col gap-0 overflow-auto no-scrollbar"
+        className="max-w-[450px] w-[90%] flex flex-col gap-0 overflow-auto no-scrollbar"
         close={() => {}}
       >
         <DialogHeader className="text-xl">Edit Max Leverage</DialogHeader>
